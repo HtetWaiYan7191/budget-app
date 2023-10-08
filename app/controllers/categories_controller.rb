5ed7f50d404c89a0e266f4d369d4fe6a727dca19
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-08 12:30:31 +0630

    Implement the create category method in categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,41 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
   
   def new
     @category = Category.new
+    @options = [
+      ["🍔"],
+    ["🛒"],
+    ["🚗"],
+    ["🐶"],
+    ["📚"],
+    ["💻"],
+    ["🎁"],
+    ["🏠"],
+    ["🏥"],
+    ["🎬"],
+    ["👔"],
+    ["🎓"],
+    ]
   end
 
   def create
-    
+    @category = Category.new(categories_params)
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created'
+    else
+      redirect_to new_category_path, notice: 'Cannot create a new category'
+    end
   end
 
   def splash
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(:name, :icon).merge(user_id: current_user.id)
   end
 end
