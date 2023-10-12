e6ab5a23439e33b2ea1bc3f29b56b4db5012bb07
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-12 16:13:55 +0630

    Add create purchase function in controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,9 @@
 class PurchasesController < ApplicationController
   def index
-    @purchases = Purchase.all
+    @category = Category.find(params[:category_id])
+    @purchases = @category.purchases
+    @purchases = @purchases.sort { |a, b| b.created_at <=> a.created_at }
+    @total  = @purchases.map(&:amount).sum
   end
 
   def new
@@ -8,8 +11,28 @@ class PurchasesController < ApplicationController
   end
 
   def create
+    @purchase = Purchase.new(purchases_params)
+    @category = Category.find(params[:category_id])
+      
+  
+    if @purchase.save
+      CategoryPurchase.create(category_id: @category.id, purchase_id: @purchase.id)
+      redirect_to category_purchases_path(category_id: @category.id), notice: 'Purchase was successfully created.'
+    else
+      redirect_to new_category_purchase_path(category_id: @category.id), notice: 'Purchase cannot create '
+     
+      
+    end
+
   end
+  
 
   def destroy
+  end
+
+  private 
+  
+  def purchases_params
+    params.require(:purchase).permit(:name, :amount).merge(author_id: current_user.id)
   end
 end
