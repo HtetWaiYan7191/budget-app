5a74e01f46e6fdcfeced60afdabe905d94c83593
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-16 16:01:52 +0630

    Fix rubocop errors

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,4 +1,5 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @category = Category.find(params[:category_id])
     @purchases = @category.purchases
