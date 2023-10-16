193010a571f39a35d76a15f46cf1c6a216f190d1
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-10-16 15:32:45 +0630

    fix rubocop errors

--- spec/features/categories_purchases_index_spec.rb
+++ spec/features/categories_purchases_index_spec.rb
@@ -1,27 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'category purchases index ' do
-    before :each do
-        login_user
-    end
+  before :each do
+    login_user
+  end
 
-    it 'see the category name ' do
-        visit category_purchases_path(@category.id)
-        expect(page).to have_content(@category.name)
-    end
+  it 'see the category name ' do
+    visit category_purchases_path(@category.id)
+    expect(page).to have_content(@category.name)
+  end
 
-    it 'see the purchaes create date ' do
-        visit category_purchases_path(@category.id)
-        expect(page).to have_content(@purchase.created_at.strftime('%d-%m-%Y'))
-    end
+  it 'see the purchaes create date ' do
+    visit category_purchases_path(@category.id)
+    expect(page).to have_content(@purchase.created_at.strftime('%d-%m-%Y'))
+  end
 
-    it 'see the total price' do
-        visit category_purchases_path(@category.id)
-        expect(page).to have_content(@purchases.sum(:amount))
-    end
+  it 'see the total price' do
+    visit category_purchases_path(@category.id)
+    expect(page).to have_content(@purchases.sum(:amount))
+  end
 
-    it 'see the total amount text 'do 
+  it 'see the total amount text ' do
     visit category_purchases_path(@category.id)
     expect(page).to have_content('Total Amount:')
-    end
+  end
 end
